ed8545bfdcbacdb8b85e7a3bda8eb7ffc43b92be
Houssam Haidar <houssam@sdelements.ca>
2012-03-09 15:15:04 -0600

    Cleaned up and bootstrapified login page.

---
+++ media/js/login.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#login .toggle-register').click(function() {
+        var $register = $('#login .register');
+        var $form = $('#login form');
+        var $link = $('#login .toggle-register');
+        if ($register.is(':visible')) {
+            $form.attr('action', '/login');
+            $register.slideUp();
+            $link.text('New? Create an account.');
+        } else {
+            $form.attr('action', '/register');
+            $register.slideDown();
+            $link.text('Actually, I have an account.');
+        }
+    });
+})
