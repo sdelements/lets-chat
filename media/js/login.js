fea9a3f3ea3891a6619c59a89fe72445e4db2f6f
Jeff Bain <Jeff@JeffBain.ca>
2012-03-12 22:19:08 -0400

    Appearance Tweaks on Login page
        Change Confirm Password label to actually say Confirm Password
        Make login button change to register when form expanded

--- media/js/login.js
+++ media/js/login.js
@@ -3,14 +3,17 @@ $(function () {
         var $register = $('#login .register');
         var $form = $('#login form');
         var $link = $('#login .toggle-register');
+        var $button = $('#login .btn')
         if ($register.is(':visible')) {
             $form.attr('action', '/login');
             $register.slideUp();
             $link.text('New? Create an account.');
+            $button.val('Login')
         } else {
             $form.attr('action', '/register');
             $register.slideDown();
             $link.text('Actually, I have an account.');
+            $button.val('Register')
         }
     });
 });
