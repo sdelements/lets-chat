d1ee5165ee877ffabcd90f265ee2eac1ac2a33e3
Houssam Haidar <houssam@sdelements.com>
2012-11-06 20:21:31 -0600

    Remove tabs from file.js

--- app/models/file.js
+++ app/models/file.js
@@ -4,25 +4,25 @@ var ObjectId = Schema.ObjectId;
 
 var FileSchema = new Schema({
     room: {
-		type: Schema.ObjectId,
-		ref: 'Room',
+        type: Schema.ObjectId,
+        ref: 'Room',
         required: true
-	},
-	owner: {
-		type: Schema.ObjectId,
-		ref: 'User',
+    },
+    owner: {
+        type: Schema.ObjectId,
+        ref: 'User',
         required: true
-	},
+    },
     name: {
         type: String,
         required: true,
         trim: true
     },
-	type: {
+    type: {
         type: String,
         required: true
     },
-	size: {
+    size: {
         type: Number,
         required: true
     },
