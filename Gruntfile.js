1f22c0463f56f55050062db5354f45c233ae8242
Houssam Haidar <houssam@sdelements.com>
2014-03-30 01:24:53 -0500

    Add Gruntfile, less and a bunch of other stuff

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        bower: {
+            install: {
+                options: {
+                    install: true,
+                    cleanTargetDir: true,
+                    verbose: true,
+                    targetDir: 'static_common/js/vendor',
+                    layout: 'byComponent',
+                }
+            }
+        },
+        less: {
+            build: {
+                files: {
+                    'media/css/style.css': 'media/less/style.less'
+                }
+            }
+        },
+        watch: {
+            less: {
+                files: ['media/less/**/*.less'],
+                tasks: ['less'],
+                options: {
+                    atBegin: true,
+                    spawn: false
+                },
+            },
+        },
+    });
+    grunt.task.registerTask('default', ['bower', 'less']);
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+};
