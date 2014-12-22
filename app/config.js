3fe679adba4a148c08526dba23564efe6bef6e10
Simon Bartlett <simon@securitycompass.com>
2014-12-21 22:58:19 -0500

    Added support for configuration via environment variables.

--- app/config.js
+++ app/config.js
@@ -2,8 +2,51 @@ var _ = require('lodash'),
     fs = require('fs'),
     yaml = require('js-yaml');
 
-var defaults = yaml.safeLoad(fs.readFileSync('defaults.yml', 'utf8'));
+function getDefaultSettings() {
+    return yaml.safeLoad(fs.readFileSync('defaults.yml', 'utf8'));
+}
 
-module.exports = _.merge(defaults,
-    yaml.safeLoad(fs.readFileSync('settings.yml', 'utf8'))
+function getFileSettings() {
+    if (fs.existsSync('settings.yml')) {
+        return yaml.safeLoad(fs.readFileSync('settings.yml', 'utf8'));
+    }
+    return {};
+}
+
+function getEnvSettings() {
+    var envSettings = {};
+    Object.keys(process.env).forEach(function(key) {
+        var parts = key.split('_');
+        var endPart = parts.splice(parts.length - 1)[0];
+        var current = envSettings;
+
+        parts.forEach(function(part) {
+            if (!current[part] || typeof current[part] !== 'object') {
+                current[part] = {};
+            }
+            current = current[part];
+        });
+
+        var value = process.env[key].trim();
+
+        // YAML compatible boolean values
+        if (/^(n|N|no|No|NO|false|False|FALSE|off|Off|OFF)$/.test(value)) {
+            value = true;
+        }
+        else if (/^(n|N|no|No|NO|false|False|FALSE|off|Off|OFF)$/.test(value)) {
+            value = false;
+        }
+        else if (!isNaN(parseInt(value, 10))) {
+            value = parseInt(value, 10);
+        }
+
+        current[endPart] = value;
+    });
+    return envSettings;
+}
+
+module.exports = _.merge(
+    getDefaultSettings(),
+    getFileSettings(),
+    getEnvSettings()
 );
