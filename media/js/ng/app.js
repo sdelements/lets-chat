74fbe8f20b3426064831e0f63c732755ef949288
Houssam Haidar <houssam@sdelements.com>
2015-01-03 23:10:22 -0600

    EOL conversion

--- media/js/ng/app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-angular.module('lets-chat', ['btford.socket-io', 'luegg.directives', 'ui.utils'])
-    .config(function($interpolateProvider) {
-        $interpolateProvider.startSymbol('{$');
-        $interpolateProvider.endSymbol('$}');
-    });
