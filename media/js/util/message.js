a45978fc0cea5422edd8338f66170779d6b8ab3f
Simon Bartlett <simon@securitycompass.com>
2015-01-08 14:05:12 -0500

    Cleaned up message formatting, and made emote rendering more efficient

--- media/js/util/message.js
+++ media/js/util/message.js
@@ -1,7 +1,9 @@
 'use strict';
 
 if (typeof window !== 'undefined' && typeof exports === 'undefined') {
-    if (typeof window.utils !== 'object') window.utils = {};
+    if (typeof window.utils !== 'object') {
+        window.utils = {};
+    }
 }
 
 if (typeof exports !== 'undefined') {
@@ -12,32 +14,73 @@ if (typeof exports !== 'undefined') {
     //
     // Message Text Formatting
     //
-    exports.format = function(text, extras) {
+
+    function trim(text) {
+        return text.trim();
+    }
+
+    function mentions(text) {
+        var mentionPattern = /\B@(\w+)(?!@)\b/g;
+        return text.replace(mentionPattern, '<strong>@$1</strong>');
+    }
+
+    function links(text) {
         var imagePattern = /^\s*((https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;'"!()]*[-A-Z0-9+&@#\/%=~_|][.](jpe?g|png|gif))\s*$/i,
-            linkPattern = /((https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;'"!()]*[-A-Z0-9+&@#\/%=~_|])/ig,
-            mentionPattern = /\B@(\w+)(?!@)\b/g;
-        text = text.trim();
-        text = text.replace(mentionPattern, '<strong>@$1</strong>');
+        linkPattern = /((https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;'"!()]*[-A-Z0-9+&@#\/%=~_|])/ig;
+
         if (imagePattern.test(text)) {
-            text = text.replace(imagePattern, function(url) {
+            return text.replace(imagePattern, function(url) {
                 url = _.escape(url);
-                return '<a class="thumbnail" href="' + url + '" target="_blank"><img src="' + url + '" alt="Pasted Image" /></a>'
+                return '<a class="thumbnail" href="' + url +
+                       '" target="_blank"><img src="' + url +
+                       '" alt="Pasted Image" /></a>';
             });
         } else {
-            text = text.replace(linkPattern, function(url) {
+            return text.replace(linkPattern, function(url) {
                 url = _.escape(url);
                 return '<a href="' + url + '" target="_blank">' + url + '</a>';
             });
         }
-        _.each(extras.emotes, function(emote) {
-            var regex = new RegExp('\\B(:' + emote.emote + ':?)[\\b]?', 'i');
-            text = text.replace(regex, function() {
-                return '<img class="emote" src="' + _.escape(emote.image) + '" title=":'+ _.escape(emote.emote) + ':" alt=":' + _.escape(emote.emote) + ':" width="50" height="50" />';
+    }
+
+    function emotes(text, extras) {
+        var regex = new RegExp('\\B(:[a-z0-9]+:?)[\\b]?', 'i');
+
+        return text.replace(regex, function(group) {
+            var key = group.split(':')[1];
+            var emote = _.find(extras.emotes, function(emote) {
+                return emote.emote === key;
             });
+
+            if (!emote) {
+                return group;
+            }
+
+            return '<img class="emote" src="' + _.escape(emote.image) + '" title=":'+ _.escape(emote.emote) + ':" alt=":' + _.escape(emote.emote) + ':" width="50" height="50" />';
         });
+    }
+
+    function replacements(text, extras) {
         _.each(extras.replacements, function(replacement) {
             text = text.replace(new RegExp(replacement.regex, 'ig'), replacement.template);
         });
         return text;
     }
+
+    exports.format = function(text, extras) {
+        var pipeline = [
+            trim,
+            mentions,
+            links,
+            emotes,
+            replacements
+        ];
+
+        _.each(pipeline, function(func) {
+            text = func(text, extras);
+        });
+
+        return text;
+    };
+
 })(typeof exports === 'undefined' ? window.utils.message = {} : exports);
