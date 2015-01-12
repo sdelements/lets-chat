af53718412881ce16d08f44492644429214d1893
Houssam Haidar <houssam@sdelements.com>
2015-01-12 18:39:36 -0500

    Add HTTPS support

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var _ = require('lodash'),
     colors = require('colors'),
     express = require('express.io'),
     expressMiddleware = require('express.io-middleware'),
+    http = require('http'),
     nunjucks = require('nunjucks'),
     mongoose = require('mongoose'),
     MongoStore = require('connect-mongo')(express),
@@ -26,7 +27,15 @@ var auth = require('./app/auth/index'),
 //
 // Express.io Setup
 //
-var app = express().http().io();
+var app;
+if (settings.server.https) {
+     app = express().https({
+        key: fs.readFileSync(settings.server.https.key),
+        cert: fs.readFileSync(settings.server.https.cert)
+    }).io();
+} else {
+    app = express().http().io();
+}
 expressMiddleware(app);
 
 // Session
@@ -77,12 +86,9 @@ _.each(controllers, function(controller) {
 //
 // Mongo
 //
-mongoose.connect(settings.database.uri);
 
 mongoose.connection.on('error', function (err) {
-    if (err) {
-        console.warn(err);
-    }
+    throw new Error(err);
 });
 
 mongoose.connection.on('disconnected', function() {
@@ -92,12 +98,31 @@ mongoose.connection.on('disconnected', function() {
 //
 // Go Time
 //
-app.listen(settings.server.port || 5000);
-
-if (settings.xmpp.enable) {
-    var xmpp = require('./app/xmpp/index');
-    xmpp(core);
-}
+mongoose.connect(settings.database.uri, function(err) {
+    if (err) throw err;
+    //
+    // HTTPS / HTTP
+    //
+    if (settings.server.https) {
+        // Create an HTTP -> HTTPS redirect server
+        var redirectServer = express(),
+            port = settings.server.https.port || 5001;
+        redirectServer.get('*', function(req, res) {
+            res.redirect('https://' + req.host + ':' + port + req.path)
+        });
+        http.createServer(redirectServer).listen(settings.server.port || 5000);
+        app.listen(port);
+    } else {
+        app.listen(settings.server.port || 5000);
+    }
+    //
+    // XMPP
+    //
+    if (settings.xmpp.enable) {
+        var xmpp = require('./app/xmpp/index');
+        xmpp(core);
+    }
+});
 
 var art = fs.readFileSync('./app/misc/art.txt', 'utf8');
 console.log('\n' + art + '\n\n' + 'Release ' + psjon.version.yellow + '\n');
