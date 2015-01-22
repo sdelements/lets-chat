723e56d3f46343603585d20e6b05761cb8962eb4
Simon Bartlett <simon@securitycompass.com>
2015-01-21 19:15:55 -0500

    Hide SweetAlert's confirmation button on successful login

--- media/js/login.js
+++ media/js/login.js
@@ -3,6 +3,78 @@
 //
 
 +function() {
+
+    function onSubmit(form, callbacks) {
+        var $form = $(form);
+        var setMessage = function(text, type, delay) {
+            $form.find('.response').each(function () {
+                var isError = type === 'error' ? true : false;
+                $(this).text(text);
+                $(this).toggleClass('alert-danger', isError)
+                .toggleClass('alert-success', !isError)
+                .stop(true, true)
+                .fadeIn(100);
+                if (delay) {
+                    $(this).delay(delay).fadeOut(200);
+                }
+            });
+        };
+        $.ajax({
+            type: 'POST',
+            url: $form.attr('action'),
+            data: $form.serialize(),
+            dataType: 'json',
+            complete: getLoginCallback($form)
+        });
+    }
+
+    function getLoginCallback($form) {
+        return function(res) {
+            switch(res.status) {
+                case 200:
+                case 201:
+                    var removeButtonCss = '<style>' +
+                        '.sweet-alert .confirm{display: none;}' +
+                        '.sweet-alert p{margin-bottom:20px;}</style>';
+
+                    $('body').append(removeButtonCss);
+
+                    swal('Success', res.responseJSON.message, 'success');
+
+                    if ($form.data('refresh')) {
+                        setTimeout(function() {
+                            window.location = '/';
+                        }, 1000);
+                        return;
+                    }
+                    $form[0].reset();
+                    $('.lcb-show-box:visible').click();
+                    break;
+
+                case 401:
+                    swal('Woops.',
+                         'Your username or password is not correct',
+                         'warning');
+                    break;
+
+                case 403:
+                    swal('Woops.',
+                         'Your account is locked',
+                         'warning');
+                    break;
+
+                default:
+                    swal('Woops.',
+                         'A server error has occured',
+                         'error');
+                    break;
+            }
+            // $indicator.removeClass('loading');
+            // Clear some inputs
+            $form.find('[data-clear="true"]').val('');
+        };
+    }
+
     $(function() {
         // JVFloat
         $('input[placeholder]').jvFloat();
@@ -27,56 +99,7 @@
         // Validation
         $('form.validate').each(function() {
             $(this).validate({
-                submitHandler: function(form, callbacks) {
-                    var $form = $(form);
-                    var setMessage = function(text, type, delay) {
-                        $form.find('.response').each(function () {
-                            var isError = type == 'error' ? true : false;
-                            $(this).text(text);
-                            $(this).toggleClass('alert-danger', isError)
-                                .toggleClass('alert-success', !isError)
-                                .stop(true, true)
-                                .fadeIn(100);
-                            if (delay) {
-                                $(this).delay(delay).fadeOut(200);
-                            }
-                        });
-                    };
-                    $.ajax({
-                        type: 'POST',
-                        url: $form.attr('action'),
-                        data: $form.serialize(),
-                        dataType: 'json',
-                        complete: function(res) {
-                            switch(res.status) {
-                                case 200:
-                                case 201:
-                                    swal('Success', res.responseJSON.message, 'success');
-                                    if ($form.data('refresh')) {
-                                        setTimeout(function() {
-                                            window.location = '/';
-                                        }, 1000);
-                                        return;
-                                    }
-                                    $form[0].reset();
-                                    $('.lcb-show-box:visible').click();
-                                    break;
-                                case 401:
-                                    swal('Woops.', 'Your username or password is not correct', 'warning');
-                                    break;
-                                case 403:
-                                    swal('Woops.', 'Your account is locked', 'warning');
-                                    break;
-                                default:
-                                    swal('Woops.', 'A server error has occured', 'error');
-                                    break;
-                            }
-                            // $indicator.removeClass('loading');
-                            // Clear some inputs
-                            $form.find('[data-clear="true"]').val('');
-                        }
-                    });
-                }
+                submitHandler: onSubmit
             });
         });
     });
