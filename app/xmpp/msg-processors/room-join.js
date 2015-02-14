eca3184b0a85242b49bfdbe18694f91eac76df27
Romain Maneschi <romain@maneschi.fr>
2015-02-14 15:36:04 +0100

    create room if user join unknown room
    Follow xmpp spec http://xmpp.org/extensions/xep-0045.html#createroom

--- app/xmpp/msg-processors/room-join.js
+++ app/xmpp/msg-processors/room-join.js
@@ -7,6 +7,122 @@ var _ = require('lodash'),
     settings = require('./../../config'),
     helper = require('./../helper');
 
+function handleJoin(room, cb) {
+    var connection = this.client.conn;
+    var username = connection.user.username;
+
+    var proom = this.core.presence.rooms.get(room._id);
+    var usernames = proom ? proom.getUsernames() : [];
+
+    // User's own presence must be last - and be their nickname
+    var i = usernames.indexOf(username);
+    if (i > -1) {
+        usernames.splice(i, 1);
+    }
+    usernames.push(connection.nickname);
+
+    var presences = usernames.map(function(username) {
+
+        var presence = this.Presence({
+            from: helper.getRoomJid(room.slug, username)
+        });
+
+        presence
+            .c('x', {
+                xmlns:'http://jabber.org/protocol/muc#user'
+            })
+            .c('item', {
+                jid: helper.getUserJid(username),
+                affiliation: 'none',
+                role: 'participant'
+            });
+
+        return presence;
+
+    }, this);
+
+    var subject = this.Message({
+        type: 'groupchat'
+    });
+
+    subject.c('subject').t(room.name + ' | ' + room.description);
+
+    var xNode = _.find(this.request.children, function(child) {
+        return child.name === 'x';
+    });
+
+    var historyNode;
+    if (xNode) {
+        var historyNode = _.find(xNode.children, function(child) {
+            return child.name === 'history';
+        });
+    }
+
+    if (!historyNode ||
+        historyNode.attrs.maxchars === 0 ||
+        historyNode.attrs.maxchars === '0') {
+            // Send no history
+            this.core.presence.join(connection, room._id, room.slug);
+            return cb(null, presences, subject);
+    }
+
+    var query = {
+        room: room._id
+    };
+
+    if (historyNode.attrs.since) {
+        query.since = moment(historyNode.attrs.since).utc().toDate();
+    }
+
+    if (historyNode.attrs.seconds) {
+        query.since = moment()
+            .subtract(historyNode.attrs.since, 'seconds')
+            .utc()
+            .toDate();
+    }
+
+    if (historyNode.attrs.maxstanzas) {
+        query.take = historyNode.attrs.maxstanzas;
+    }
+
+    this.core.messages.list(query, function(err, messages) {
+
+        messages.reverse();
+
+        var msgs = messages.map(function(msg) {
+
+            var stanza = new Stanza.Message({
+                id: msg._id,
+                type: 'groupchat',
+                to: helper.getRoomJid(room.slug),
+                from: helper.getRoomJid(room.slug, msg.owner.username)
+            });
+
+            stanza.c('body').t(msg.text);
+
+            stanza.c('delay', {
+                xmlns: 'urn:xmpp:delay',
+                from: helper.getRoomJid(room.slug),
+                stamp: msg.posted.toISOString()
+            });
+
+            stanza.c('addresses', {
+                xmlns: 'http://jabber.org/protocol/address'
+            }).c('address', {
+                type: 'ofrom',
+                jid: helper.getUserJid(msg.owner.username)
+            });
+
+            return stanza;
+
+        }, this);
+
+        this.core.presence.join(connection, room._id, room.slug);
+        cb(null, presences, msgs, subject);
+
+    }.bind(this));
+};
+
 module.exports = MessageProcessor.extend({
 
     if: function() {
@@ -25,127 +141,26 @@ module.exports = MessageProcessor.extend({
         // TODO: Do we need to track nickname for each individual room?
         connection.nickname = nickname;
 
-        var xNode = _.find(this.request.children, function(child) {
-            return child.name === 'x';
-        });
-
-        var historyNode;
-        if (xNode) {
-            var historyNode = _.find(xNode.children, function(child) {
-                return child.name === 'history';
-            });
-        }
-
         this.core.rooms.slug(roomSlug, function(err, room) {
             if (err) {
                 return cb(err);
             }
             if (!room) {
-                return cb(null);
+                var options = {
+                    owner: connection.user.id,
+                    name: roomSlug,
+                    slug: roomSlug,
+                    description: ''
+                };
+                this.core.rooms.create(options, function(err, room) {
+                    if (err) {
+                        return cb(err);
+                    }
+                    handleJoin.bind(this)(room, cb);
+                }.bind(this));
+            } else {
+                handleJoin.bind(this)(room, cb);
             }
-
-            var username = connection.user.username;
-
-            var proom = this.core.presence.rooms.get(room._id);
-            var usernames = proom ? proom.getUsernames() : [];
-
-            // User's own presence must be last - and be their nickname
-            var i = usernames.indexOf(username);
-            if (i > -1) {
-                usernames.splice(i, 1);
-            }
-            usernames.push(nickname);
-
-            var presences = usernames.map(function(username) {
-
-                var presence = this.Presence({
-                    from: helper.getRoomJid(room.slug, username)
-                });
-
-                presence
-                    .c('x', {
-                        xmlns:'http://jabber.org/protocol/muc#user'
-                    })
-                    .c('item', {
-                        jid: helper.getUserJid(username),
-                        affiliation: 'none',
-                        role: 'participant'
-                    });
-
-                return presence;
-
-            }, this);
-
-            var subject = this.Message({
-                type: 'groupchat'
-            });
-
-            subject.c('subject').t(room.name + ' | ' + room.description);
-
-            if (!historyNode ||
-                historyNode.attrs.maxchars === 0 ||
-                historyNode.attrs.maxchars === '0') {
-                    // Send no history
-                    this.core.presence.join(connection, room._id, room.slug);
-                    return cb(null, presences, subject);
-            }
-
-            var query = {
-                room: room._id
-            };
-
-            if (historyNode.attrs.since) {
-                query.since = moment(historyNode.attrs.since).utc().toDate();
-            }
-
-            if (historyNode.attrs.seconds) {
-                query.since = moment()
-                    .subtract(historyNode.attrs.since, 'seconds')
-                    .utc()
-                    .toDate();
-            }
-
-            if (historyNode.attrs.maxstanzas) {
-                query.take = historyNode.attrs.maxstanzas;
-            }
-
-            this.core.messages.list(query, function(err, messages) {
-
-                messages.reverse();
-
-                var msgs = messages.map(function(msg) {
-
-                    var stanza = new Stanza.Message({
-                        id: msg._id,
-                        type: 'groupchat',
-                        to: helper.getRoomJid(room.slug),
-                        from: helper.getRoomJid(room.slug, msg.owner.username)
-                    });
-
-                    stanza.c('body').t(msg.text);
-
-                    stanza.c('delay', {
-                        xmlns: 'urn:xmpp:delay',
-                        from: helper.getRoomJid(room.slug),
-                        stamp: msg.posted.toISOString()
-                    });
-
-                    stanza.c('addresses', {
-                        xmlns: 'http://jabber.org/protocol/address'
-                    }).c('address', {
-                        type: 'ofrom',
-                        jid: helper.getUserJid(msg.owner.username)
-                    });
-
-                    return stanza;
-
-                }, this);
-
-                this.core.presence.join(connection, room._id, room.slug);
-                cb(null, presences, msgs, subject);
-
-            }.bind(this));
-
         }.bind(this));
     }
 
