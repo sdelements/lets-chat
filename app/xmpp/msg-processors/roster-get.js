0bdd011f4b563f05ffc17883522636124476c24b
Simon Bartlett <simon@securitycompass.com>
2015-02-26 00:21:45 -0500

    Implement XMPP get roster request

--- app/xmpp/msg-processors/roster-get.js
+++ app/xmpp/msg-processors/roster-get.js
@@ -1,7 +1,9 @@
 'use strict';
 
-var MessageProcessor = require('./../msg-processor'),
-    settings = require('./../../config');
+var _ = require('lodash'),
+    MessageProcessor = require('./../msg-processor'),
+    settings = require('./../../config'),
+    helper = require('./../helper');
 
 module.exports = MessageProcessor.extend({
 
@@ -9,8 +11,6 @@ module.exports = MessageProcessor.extend({
         return this.request.type === 'get' && this.ns['jabber:iq:roster'];
     },
 
-    // Roster is always empty - everyone is friendless
-    // This should only be implemented if we support 1-to-1 private convos
     then: function(cb) {
         var stanza = this.Iq();
 
@@ -18,7 +18,25 @@ module.exports = MessageProcessor.extend({
             xmlns: 'jabber:iq:roster'
         });
 
-        cb(null, stanza);
+        var users = this.core.users.list({}, function(err, users) {
+            if (err) {
+                return cb(err);
+            }
+
+            _.each(users, function(user) {
+                if (user._id.equals(this.connection.user.id)) {
+                    return;
+                }
+
+                v.c('item', {
+                    jid: helper.getUserJid(user.username),
+                    name: user.displayName,
+                    subscription: 'both'
+                }).c('group').t('Friends');
+            }, this);
+
+            cb(null, stanza);
+        }.bind(this));
     }
 
 });
