6dfb56b7bd3e30eb4daefb7f6e29134ba209ab36
Simon Bartlett <simon@securitycompass.com>
2015-04-09 14:19:28 -0400

    Added marionette

--- media/js/vendor.js
+++ media/js/vendor.js
@@ -4,6 +4,8 @@
 //= require vendor/jquery-validate/jquery.validate.js
 //= require vendor/lodash/lodash.js
 //= require vendor/backbone/backbone.js
+//= require vendor/backbone.keys/backbone.keys.js
+//= require vendor/backbone.marionette/backbone.marionette.js
 //= require vendor/moment/moment.js
 //= require vendor/handlebars/handlebars.js
 //= require vendor/bootstrap/bootstrap.js
@@ -14,6 +16,5 @@
 //= require vendor/notifications/desktop-notifications.js
 //= require vendor/at/jquery.caret.js
 //= require vendor/at/jquery.atwho.js
-//= require vendor/backbone.keys/backbone.keys.js
 //= require vendor/raptorize/jquery.raptorize.js
 //= require common.js
