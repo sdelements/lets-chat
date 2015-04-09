49f408cf5080258657f39312c5ce6dff2063c486
Simon Bartlett <simon@securitycompass.com>
2015-04-09 15:51:57 -0400

    Converted message form to marionette view

--- media/js/views/room.js
+++ media/js/views/room.js
@@ -11,8 +11,6 @@
 
     window.LCB.RoomView = Backbone.View.extend({
         events: {
-            'keypress .lcb-entry-input': 'sendMessage',
-            'click .lcb-entry-button': 'sendMessage',
             'DOMCharacterDataModified .lcb-room-heading, .lcb-room-description': 'sendMeta',
             'click .lcb-room-toggle-sidebar': 'toggleSidebar',
             'click .show-edit-room': 'showEditRoom',
@@ -45,6 +43,11 @@
                 collection: this.model.messages,
                 el: this.$('.lcb-messages')
             });
+            this.composeView = new window.LCB.ComposeView({
+                el: this.$('.lcb-compose'),
+                client: this.client,
+                room: this.model
+            });
             this.usersList = new window.LCB.RoomUsersView({
                 collection: this.model.users
             });
@@ -53,6 +56,7 @@
             });
 
             this.messageView.render();
+            this.composeView.render();
             this.usersList.render();
             this.filesList.render();
 
@@ -64,147 +68,6 @@
             this.$el = $(this.template(_.extend(this.model.toJSON(), {
                 sidebar: store.get('sidebar')
             })));
-            this.atwhoMentions();
-            this.atwhoAllMentions();
-            this.atwhoRooms();
-            this.atwhoEmotes();
-        },
-        atwhoTplEval: function(tpl, map) {
-            var error;
-            try {
-                return tpl.replace(/\$\{([^\}]*)\}/g, function(tag, key, pos) {
-                    return (map[key] || '')
-                        .replace(/&/g, '&amp;')
-                        .replace(/"/g, '&quot;')
-                        .replace(/'/g, '&apos;')
-                        .replace(/</g, '&lt;')
-                        .replace(/>/g, '&gt;');
-                });
-            } catch (_error) {
-                error = _error;
-                return "";
-            }
-        },
-        getAtwhoUserFilter: function(collection) {
-            var currentUser = this.client.user;
-
-            return function filter(query, data, searchKey) {
-                var q = query.toLowerCase();
-                var results = collection.filter(function(user) {
-                    var attr = user.attributes;
-
-                    if (user.id === currentUser.id) {
-                        return false;
-                    }
-
-                    if (!attr.safeName) {
-                        attr.safeName = attr.displayName.replace(/\W/g, '');
-                    }
-
-                    var val1 = attr.username.toLowerCase();
-                    var val1i = val1.indexOf(q);
-                    if (val1i > -1) {
-                        attr.atwho_order = val1i;
-                        return true;
-                    }
-
-                    var val2 = attr.safeName.toLowerCase();
-                    var val2i = val2.indexOf(q);
-                    if (val2i > -1) {
-                        attr.atwho_order = val2i + attr.username.length;
-                        return true;
-                    }
-
-                    return false;
-                });
-
-                return results.map(function(user) {
-                    return user.attributes;
-                });
-            };
-        },
-        atwhoMentions: function () {
-
-            function sorter(query, items, search_key) {
-                return items.sort(function(a, b) {
-                    return a.atwho_order - b.atwho_order;
-                });
-            }
-
-            this.$('.lcb-entry-input')
-            .atwho({
-                at: '@',
-                tpl: '<li data-value="@${username}"><img src="https://www.gravatar.com/avatar/${avatar}?s=20" height="20" width="20" /> @${username} <small>${displayName}</small></li>',
-                callbacks: {
-                    filter: this.getAtwhoUserFilter(this.model.users),
-                    sorter: sorter,
-                    tpl_eval: this.atwhoTplEval
-                }
-            });
-        },
-        atwhoAllMentions: function () {
-            var that = this;
-
-            function filter(query, data, searchKey) {
-                var users = that.client.getUsersSync();
-                var filt = that.getAtwhoUserFilter(users);
-                return filt(query, data, searchKey);
-            }
-
-            function sorter(query, items, search_key) {
-                return items.sort(function(a, b) {
-                    return a.atwho_order - b.atwho_order;
-                });
-            }
-
-            this.$('.lcb-entry-input')
-            .atwho({
-                at: '@@',
-                tpl: '<li data-value="@${username}"><img src="https://www.gravatar.com/avatar/${avatar}?s=20" height="20" width="20" /> @${username} <small>${displayName}</small></li>',
-                callbacks: {
-                    filter: filter,
-                    sorter: sorter,
-                    tpl_eval: that.atwhoTplEval
-                }
-            });
-        },
-        atwhoRooms: function() {
-            var rooms = this.client.rooms;
-
-            function filter(query, data, searchKey) {
-                var q = query.toLowerCase();
-                var results = rooms.filter(function(room) {
-                    var val = room.attributes.slug.toLowerCase();
-                    return val.indexOf(q) > -1;
-                });
-
-                return results.map(function(room) {
-                    return room.attributes;
-                });
-            }
-
-            this.$('.lcb-entry-input')
-                .atwho({
-                    at: '#',
-                    search_key: 'slug',
-                    callbacks: {
-                        filter: filter,
-                        tpl_eval: this.atwhoTplEval
-                    },
-                    tpl: '<li data-value="#${slug}">#${slug} <small>${name}</small></li>'
-                });
-        },
-        atwhoEmotes: function() {
-            var that = this;
-            this.client.getEmotes(function(emotes) {
-                that.$('.lcb-entry-input')
-                .atwho({
-                    at: ':',
-                    search_key: 'emote',
-                    data: emotes,
-                    tpl: '<li data-value=":${emote}:"><img src="${image}" height="32" width="32" alt=":${emote}:" /> :${emote}:</li>'
-                });
-            });
         },
         goodbye: function() {
             swal('Archived!', '"' + this.model.get('name') + '" has been archived.', 'warning');
@@ -303,18 +166,6 @@
                 }
             });
         },
-        sendMessage: function(e) {
-            if (e.type === 'keypress' && e.keyCode !== 13 || e.altKey) return;
-            e.preventDefault();
-            if (!this.client.status.get('connected')) return;
-            var $textarea = this.$('.lcb-entry-input');
-            if (!$textarea.val()) return;
-            this.client.events.trigger('messages:send', {
-                room: this.model.id,
-                text: $textarea.val()
-            });
-            $textarea.val('');
-        },
         toggleSidebar: function(e) {
             e && e.preventDefault && e.preventDefault();
             // Target siblings too!
@@ -356,6 +207,182 @@
         }
     });
 
+    window.LCB.ComposeView = Marionette.ItemView.extend({
+        template: Handlebars.compile($('#template-compose').html()),
+        tagName: 'form',
+        attributes: {
+            'class': 'lcb-entry'
+        },
+        events: {
+            'keypress .lcb-entry-input': 'sendMessage',
+            'click .lcb-entry-button': 'sendMessage'
+        },
+        onRender: function() {
+            this.atwhoMentions();
+            this.atwhoAllMentions();
+            this.atwhoRooms();
+            this.atwhoEmotes();
+        },
+        sendMessage: function(e) {
+            if (e.type === 'keypress' && e.keyCode !== 13 || e.altKey) {
+                return;
+            }
+
+            e.preventDefault();
+            if (!this.options.client.status.get('connected')) {
+                return;
+            }
+
+            var $textarea = this.$('.lcb-entry-input');
+            if (!$textarea.val()) {
+                return;
+            }
+
+            this.options.client.events.trigger('messages:send', {
+                room: this.options.room.id,
+                text: $textarea.val()
+            });
+            $textarea.val('');
+        },
+        atwhoTplEval: function(tpl, map) {
+            var error;
+            try {
+                return tpl.replace(/\$\{([^\}]*)\}/g, function(tag, key, pos) {
+                    return (map[key] || '')
+                        .replace(/&/g, '&amp;')
+                        .replace(/"/g, '&quot;')
+                        .replace(/'/g, '&apos;')
+                        .replace(/</g, '&lt;')
+                        .replace(/>/g, '&gt;');
+                });
+            } catch (_error) {
+                error = _error;
+                return "";
+            }
+        },
+        getAtwhoUserFilter: function(collection) {
+            var currentUser = this.options.client.user;
+
+            return function filter(query, data, searchKey) {
+                var q = query.toLowerCase();
+                var results = collection.filter(function(user) {
+                    var attr = user.attributes;
+
+                    if (user.id === currentUser.id) {
+                        return false;
+                    }
+
+                    if (!attr.safeName) {
+                        attr.safeName = attr.displayName.replace(/\W/g, '');
+                    }
+
+                    var val1 = attr.username.toLowerCase();
+                    var val1i = val1.indexOf(q);
+                    if (val1i > -1) {
+                        attr.atwho_order = val1i;
+                        return true;
+                    }
+
+                    var val2 = attr.safeName.toLowerCase();
+                    var val2i = val2.indexOf(q);
+                    if (val2i > -1) {
+                        attr.atwho_order = val2i + attr.username.length;
+                        return true;
+                    }
+
+                    return false;
+                });
+
+                return results.map(function(user) {
+                    return user.attributes;
+                });
+            };
+        },
+        atwhoMentions: function () {
+
+            function sorter(query, items, search_key) {
+                return items.sort(function(a, b) {
+                    return a.atwho_order - b.atwho_order;
+                });
+            }
+
+            this.$('.lcb-entry-input')
+            .atwho({
+                at: '@',
+                tpl: '<li data-value="@${username}"><img src="https://www.gravatar.com/avatar/${avatar}?s=20" height="20" width="20" /> @${username} <small>${displayName}</small></li>',
+                callbacks: {
+                    filter: this.getAtwhoUserFilter(this.options.room.users),
+                    sorter: sorter,
+                    tpl_eval: this.atwhoTplEval
+                }
+            });
+        },
+        atwhoAllMentions: function () {
+            var that = this;
+
+            function filter(query, data, searchKey) {
+                var users = that.client.getUsersSync();
+                var filt = that.getAtwhoUserFilter(users);
+                return filt(query, data, searchKey);
+            }
+
+            function sorter(query, items, search_key) {
+                return items.sort(function(a, b) {
+                    return a.atwho_order - b.atwho_order;
+                });
+            }
+
+            this.$('.lcb-entry-input')
+            .atwho({
+                at: '@@',
+                tpl: '<li data-value="@${username}"><img src="https://www.gravatar.com/avatar/${avatar}?s=20" height="20" width="20" /> @${username} <small>${displayName}</small></li>',
+                callbacks: {
+                    filter: filter,
+                    sorter: sorter,
+                    tpl_eval: that.atwhoTplEval
+                }
+            });
+        },
+        atwhoRooms: function() {
+            var rooms = this.options.client.rooms;
+
+            function filter(query, data, searchKey) {
+                var q = query.toLowerCase();
+                var results = rooms.filter(function(room) {
+                    var val = room.attributes.slug.toLowerCase();
+                    return val.indexOf(q) > -1;
+                });
+
+                return results.map(function(room) {
+                    return room.attributes;
+                });
+            }
+
+            this.$('.lcb-entry-input')
+                .atwho({
+                    at: '#',
+                    search_key: 'slug',
+                    callbacks: {
+                        filter: filter,
+                        tpl_eval: this.atwhoTplEval
+                    },
+                    tpl: '<li data-value="#${slug}">#${slug} <small>${name}</small></li>'
+                });
+        },
+        atwhoEmotes: function() {
+            var that = this;
+            this.options.client.getEmotes(function(emotes) {
+                that.$('.lcb-entry-input')
+                .atwho({
+                    at: ':',
+                    search_key: 'emote',
+                    data: emotes,
+                    tpl: '<li data-value=":${emote}:"><img src="${image}" height="32" width="32" alt=":${emote}:" /> :${emote}:</li>'
+                });
+            });
+        }
+    });
+
     var MessageView = Marionette.ItemView.extend({
         template: Handlebars.compile($('#template-message').html()),
         tagName: 'li',
@@ -381,7 +408,7 @@
                 attrs['class'] += ' lcb-message-paste';
             }
 
-            return attrs;  
+            return attrs;
         },
         onRender: function() {
             var $text = this.$('.lcb-message-text');
