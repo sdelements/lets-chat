f8a568bab19786e25483ad9f46362a38952ff756
Simon Bruce <simon@blacksun.cx>
2015-04-11 22:41:07 +0100

    Embarrising, refactoring error.

--- media/js/util/message.js
+++ media/js/util/message.js
@@ -19,12 +19,12 @@ if (typeof exports !== 'undefined') {
     function encodeEntities(value) {
         return value.
             replace(/&/g, '&amp;').
-            replace(SURROGATE_PAIR_REGEXP, function(value) {
+            replace(surrogatePairRegexp, function(value) {
                 var hi = value.charCodeAt(0),
                     low = value.charCodeAt(1);
                 return '&#' + (((hi - 0xD800) * 0x400) + (low - 0xDC00) + 0x10000) + ';';
             }).
-            replace(NON_ALPHANUMERIC_REGEXP, function(value) {
+            replace(nonAlphanumericRegexp, function(value) {
                 return '&#' + value.charCodeAt(0) + ';';
             }).
             replace(/</g, '&lt;').
