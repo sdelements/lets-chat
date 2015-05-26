615ef1e5656fce8fbcb6e5ba0c7fb56951962178
heena mahour <heena393@gmail.com>
2015-05-26 18:10:16 +0530

    1:1 chat on xmpp , needs work on frontend but could be useful for blugchat

---
+++ app/xmpp/helper.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var settings = require('./../config');
+
+function getUserJid(username) {
+    if (username.indexOf('@' + settings.xmpp.host) !== -1) {
+        return username;
+    }
+    return username + '@' + settings.xmpp.host;
+}
+
+function getRoomJid(roomId, username) {
+    var jid = roomId + '@' + settings.xmpp.confhost;
+    if (username) {
+        jid += '/' + username;
+    }
+    return jid;
+}
+
+function populateVcard(presence, user) {
+    var vcard = presence.c('x', { xmlns: 'vcard-temp:x:update' });
+    var photo = vcard.c('photo');
+    if (user._image) {
+        photo.t(user._image.sha1);
+    }
+}
+
+module.exports = {
+    getUserJid: getUserJid,
+    getRoomJid: getRoomJid,
+    populateVcard: populateVcard
+}; 
