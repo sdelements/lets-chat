d7f51ea52981345eff846987e3754a090030bd18
Houssam Haidar <houssam@sdelements.com>
2015-12-14 23:26:31 -0500

    Add route for materia client

---
+++ app/controllers/materia.js
@@ -0,0 +1,26 @@
+//
+// Materia Client Controller
+//
+
+'use strict';
+
+var express = require('express.oi'),
+    path = require('path');
+
+module.exports = function() {
+
+    var app = this.app;
+
+    app.use('/materia', express.static(path.resolve(__dirname, '../../materia/public')));
+
+    app.get('/materia/*', function(req, res){
+
+        if (/^\/materia\/assets/i.test(req.url)) {
+            return next();
+        }
+
+        res.sendfile(path.resolve(__dirname, '../../materia/public/index.html'));
+
+    });
+
+};
